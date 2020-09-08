b1b15f69e231170c255e703aa47fba4d72877bcf
DCupRobot <dwjandzzs@gmail.com>
2020-09-08 14:32:20 +0800

    1

--- backend/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import ModelSerializer
-from .models import Student
-
-class StudentSerializer(ModelSerializer):
-  class Meta:
-    model = Student
-    fields = ['id','name','course','rating']
